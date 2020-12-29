867b1265011ba980ebb37ef0d14e3a26e32fc44c
daniel mestawat <dannymamusha@gmail.com>
2020-12-29 04:51:53 -0500

    fourth update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // var button = document.getElementById('#searchBtn');
 
 console.log("am i linked");
-// var tocity = document.getElementById('#city');
+// var inputValue = document.getElementById('#userText');
 // var toDayIcon = document.getElementById('#weatherIcon');
 // var toDayTemp = document.getElementById('#currentTemp');
 // var toDayHum = document.getElementById('#currentHum');
@@ -10,13 +10,13 @@ console.log("am i linked");
 
 // using moment js to get the date for current date and the next 5 days.
 
-var toDay = moment().format('MM/DD/YYYY');
+// var currentDate = moment().format('MM/DD/YYYY');
 var dayOne = moment().add(1, "days").format("M/D/YYYY"); 
 var dayTwo = moment().add(2, "days").format("M/D/YYYY");   
 var dayThree = moment().add(3, "days").format("M/D/YYYY");   
 var dayFour = moment().add(4, "days").format("M/D/YYYY");
 var dayFive = moment().add(5, "days").format("M/D/YYYY");     
-$("#currentDate").append(toDay);
+// $("#currentDate").append(toDay);
 $("#secondDate").append(dayOne);
 $("#thirdDate").append(dayTwo);
 $("#fourthDate").append(dayThree);
@@ -27,46 +27,76 @@ $("#sixDate").append(dayFive);
 
 
 
-
-
-
-$("#searchBtn").on("click", function(event) {
-
-    event.preventDefault();
-      var userInput = $("#userText").val();
+  $('#searchBtn').click(function (e) { 
+    e.preventDefault();
     
-      var APIKey = "1d2da0608b16fe23a0cfaff526bcd4fb";
+    
+    var city = $("#userText").val();
+    if(city !=''){
+      
+      
 
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=userInput+"&appid="+ APIKey;
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    // "q=London&appid=" + APIKey;
+      var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=1d2da0608b16fe23a0cfaff526bcd4fb";
+      
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        // $(".wind").text("Wind Speed: " + response.wind.speed);
-        // $(".humidity").text("Humidity: " + response.main.humidity);
+      
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(data) {
         
-        // // Convert the temp to fahrenheit
-        // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var thisDay = show(data);
 
-        // // add temp content to html
-        // $(".temp").text("Temperature (K) " + response.main.temp);
-        // $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+        $("#currentDate").append(currentDate);
+        $("#toDay").html(thisDay);
+        
+        
+        
+
+        $("#userText").val('');
 
       })
-});
+      
+    }else{
+      $("#error").html('field cannot be empty!');
+    }
+  });
+  
+
+function show(data){
+  return  "<h5> "+ data.name +"</h5>" +
+          "<h5> "+ moment().format('MM/DD/YYYY') +"</h5>" +
+        //  "<img>" + data.weather.iconId + "</img>"  +
+          "<p>temperature: "+ data.main.temp +"<p>" +
+          "<p>humidity: "+ data.main.humidity +"</p>" +
+          "<h5>wind: "+ data.wind.speed +"</h5>" ;
+          // <p id="currentUv"></p>
+  
+
+}
+
+
+
+        
+        
+        
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function forcast(data){
+  return "<h5> "+ moment().add(1, "days").format('MM/D/YYYY') +"</h5>" ;
+}
